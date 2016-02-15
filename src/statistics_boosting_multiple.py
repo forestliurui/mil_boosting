9bbbc974b7e9c5d20aa54cfbd69b63b07b2efc4b
Rui Liu <rui@chronos.eecs.cwru.edu>
2016-02-15 16:55:38 -0500

    modify the results_table_generator script statistics_boosting_multiple.py so that it can take care of multiple parameter sets (i.e. boosters) for each dataset

--- src/statistics_boosting_multiple.py
+++ src/statistics_boosting_multiple.py
@@ -12,7 +12,7 @@ import sqlite3
 import yaml
 import numpy as np
 
-def compute_statistics(configuration_file, results_directory, outputfile):
+def compute_statistics(configuration_file, results_directory, outputfile_raw):
 
 	
 	statistics_name=['train_bag_AUC', 'train_bag_accuracy', 'train_bag_balanced_accuracy', 'train_instance_AUC', 'train_instance_accuracy', 'train_instance_balanced_accuracy', 'test_bag_AUC', 'test_bag_accuracy', 'test_bag_balanced_accuracy', 'test_instance_AUC', 'test_instance_accuracy', 'test_instance_balanced_accuracy']
@@ -29,56 +29,68 @@ def compute_statistics(configuration_file, results_directory, outputfile):
 	     if	dataset_name == 'trx':
 		continue
 		
-	     line=dataset_name
-	     line+= ','
-	     line+= (','.join(statistics_name) )
-	     line+= '\n'
- 	     with open(outputfile, 'a+') as f:
+	     dataset_result_path=results_directory+'/mi_kernels/'+ dataset_name+'.db'
+	     conn=sqlite3.connect(dataset_result_path)
+		
+	     c=conn.cursor()
+
+	     parameter_set_id_names = []
+	     string_to_be_exe = 'select * from parameter_sets'
+	     for row in c.execute(string_to_be_exe):
+		parameter_set_id_names.append(row)
+
+             for (parameter_set_id, parameter_set_name) in parameter_set_id_names:
+		outputfile = parameter_set_name+outputfile_raw
+		
+	     	line=dataset_name
+	     	line+= ','
+	     	line+= (','.join(statistics_name) )
+	     	line+= '\n'
+ 	     	with open(outputfile, 'a+') as f:
                 		f.write(line)
 
 
              
 		
-	     dataset_result_path=results_directory+'/mi_kernels/'+ dataset_name+'.db'
-	     conn=sqlite3.connect(dataset_result_path)
-		
-	     c=conn.cursor()
-	     #for row in c.execute('select * from statistic_names'):
-	     #print row  #row is of type tuple
+
+	     	#for row in c.execute('select * from statistic_names'):
+	     	#print row  #row is of type tuple
 	
 
 		
 		
-	     boosting_rounds_list=[]
-	     string_to_be_exe = 'select boosting_rounds from statistics_boosting '
-	     for row in c.execute(string_to_be_exe):
-		boosting_rounds_list.append(row[0])
-	     iter_max_boosting=max(boosting_rounds_list)
+	     	boosting_rounds_list=[]
+	     	string_to_be_exe = 'select boosting_rounds from statistics_boosting where parameter_set_id = %d' % parameter_set_id
+	     	for row in c.execute(string_to_be_exe):
+			boosting_rounds_list.append(row[0])
+		if len(boosting_rounds_list) == 0:
+			continue
+	     	iter_max_boosting=max(boosting_rounds_list)
 
-	     for boosting_round in range(1,iter_max_boosting+1):
-		line=('%d' % boosting_round)
-		for statistic_name in statistics_name:
-
-
-			#import pdb;pdb.set_trace()
-			string_to_be_exe = 'select statistic_name_id from statistic_names where statistic_name = "%s" ' % statistic_name
-
-			c.execute(string_to_be_exe)
-			stat_id=c.fetchone()[0]
+	     	for boosting_round in range(1,iter_max_boosting+1):
+			line=('%d' % boosting_round)
+			for statistic_name in statistics_name:
 
 
-			statistic_value_list=[]
-			string_to_be_exe = 'select  statistic_value from statistics_boosting where statistic_name_id = %d and boosting_rounds = %d' % (stat_id, boosting_round)
+				#import pdb;pdb.set_trace()
+				string_to_be_exe = 'select statistic_name_id from statistic_names where statistic_name = "%s" ' % statistic_name
 
-			for row in c.execute(string_to_be_exe):
-				statistic_value_list.append(row[0])
+				c.execute(string_to_be_exe)
+				stat_id=c.fetchone()[0]
+
+
+				statistic_value_list=[]
+				string_to_be_exe = 'select  statistic_value from statistics_boosting where statistic_name_id = %d and boosting_rounds = %d and parameter_set_id = %d' % (stat_id, boosting_round, parameter_set_id)
+
+				for row in c.execute(string_to_be_exe):
+					statistic_value_list.append(row[0])
 
 			
-			line += (',%f' % np.average(statistic_value_list)  )
-		line +='\n'
+				line += (',%f' % np.average(statistic_value_list)  )
+			line +='\n'
 		
 						
-		with open(outputfile, 'a+') as f:
+			with open(outputfile, 'a+') as f:
                 		f.write(line)
 
 if __name__ == '__main__':
