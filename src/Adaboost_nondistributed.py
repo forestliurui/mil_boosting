4208c73a63910585f49474519d249770482abd3c
Rui Liu <rui@chronos.eecs.cwru.edu>
2016-03-28 16:35:46 -0400

    change Adaboost_nondistributed.py so that predition stage has been sped up

--- src/Adaboost_nondistributed.py
+++ src/Adaboost_nondistributed.py
@@ -32,6 +32,12 @@ class AdaBoost(object):
 		self.parameters = parameters
 		self.ensemble_classifier = AdaBoostClassifier(WEAK_CLASSIFIERS[self.weak_classifier_name](**self.parameters), algorithm="SAMME", n_estimators = self.max_iter_boosting)
 
+		self.X_bags_test = None
+		self.X_bags = None
+		self.y_labels = None
+		self.X_instances = None
+
+		self.instance_labels_generated_from_bag_labels = None
 
 	def fit(self, X_bags, y_labels):
 		'''
@@ -59,10 +65,20 @@ class AdaBoost(object):
 			
 
 			instance_labels_generated_from_bag_labels=[y_labels[bag_index]*np.ones((1, num_instance_each_bag[bag_index]))[0] for bag_index in range(num_bags)  ]
-			instance_labels_generated_from_bag_labels=np.hstack((instance_labels_generated_from_bag_labels))		
+			instance_labels_generated_from_bag_labels=np.hstack((instance_labels_generated_from_bag_labels))
+
+			self.X_bags = X_bags
+			self.y_labels = y_labels 
+			self.X_instances = instances	
+			self.instance_labels_generated_from_bag_labels = instance_labels_generated_from_bag_labels		
 		else:
 			instances = X_bags
 			instance_labels_generated_from_bag_labels = y_labels
+
+			self.instance_labels_generated_from_bag_labels = instance_labels_generated_from_bag_labels
+
+			self.X_instances = instances
+
 
 		instance_labels_generated_from_bag_labels = np.array( instance_labels_generated_from_bag_labels )
 		num_instances = instances.shape[0]
@@ -77,36 +93,90 @@ class AdaBoost(object):
 		self.weak_classifiers = self.ensemble_classifier.estimators_
 		self.actual_rounds_of_boosting = len(self.weak_classifiers)
 	
-	def _predict(self, instances, iter = None):
+	def predict_train(self, iter = None, getInstPrediction = False):
+		
+		threshold = 0.5
+		if iter == None or iter > len(self.c):
+			iter = len(self.c)
+
+		staged_generator_train = self.ensemble_classifier.staged_decision_function(self.X_instances)
+
+		staged_decision_function_output_train = [x for x in staged_generator_train]  #a list of one dimensional array, contained entries with positive values for postive class and negative values for negative class
+		results = staged_decision_function_output_trainresults [iter - 1] 
+
+		if getInstPrediction:
+			return results
+		else: #get bag level predictions
+			if self.X_bags is None:
+				raise Exception("Can't get bag level prediction for training data due to the lack of training bag data")
+			else:
+				predictions_bag = get_bag_label(results, self.X_bags)
+				return predictions_bag
+
+
+	def _predict(self, instances = None, iter = None):
 		
 		if iter == None or iter > self.actual_rounds_of_boosting:
 			iter = self.actual_rounds_of_boosting
+		if instances is not None:
+			self.staged_generator_test = self.ensemble_classifier.staged_decision_function(instances)
 
-		staged_generator = self.ensemble_classifier.staged_decision_function(instances)
+			self.staged_decision_function_output_test = [x for x in self.staged_generator_test]  #a list of one dimensional array, contained entries with positive values for postive class and negative values for negative class
+			return self.staged_decision_function_output_test[iter - 1] 
+		else:
+			
+			return self.staged_decision_function_output_test[iter - 1]
 
-		staged_decision_function_output = [x for x in staged_generator]  #a list of one dimensional array, contained entries with positive values for postive class and negative values for negative class
-		return staged_decision_function_output[iter - 1] 
-
-	def predict(self, X_bags, iter = None):
+	def predict(self, X_bags = None, iter = None, getInstPrediction = False):
 		#X_bags is a list of arrays, each bag is an array in the list
 		#The row of array corresponds to instances in the bag, column corresponds to feature
 
 		#predictions_bag is the returned array of predictions which are real values 
 		
-		if iter == None or iter > self.actual_rounds_of_boosting:
-			iter = self.actual_rounds_of_boosting
+		self.c = self.alphas
+		if iter == None or iter > len(self.c):
+			iter = len(self.c)
 	
-		print "number of boosting: ",
-		print self.actual_rounds_of_boosting
-		if type(X_bags) != list:  # treat it as normal supervised learning setting
+		#print "self.c: ",
+		print len(self.c)
+		if X_bags is not None:
+			self.X_bags_test = X_bags
 
-			return self._predict(X_instances, iter)
+			if type(X_bags) != list:  # treat it as normal supervised learning setting
+				#X_bags = [X_bags[inst_index,:] for inst_index in range(X_bags.shape[0])]
+				return self._predict(instances = X_bags, iter = iter)
+				
+			else:
+
+				X_instances = np.vstack(X_bags)
+				predictions_accum = self._predict(instances = X_instances, iter =  iter)
+				if getInstPrediction:  #return the instance level predictions for the input bags
+					return predictions_accum
+				else:
+					predictions_bag = get_bag_label(predictions_accum, X_bags)
+					return predictions_bag
+
+		elif X_bags is None and self.X_bags_test is not None:
+			if type(self.X_bags_test) != list:  # treat it as normal supervised learning setting
+				#X_bags = [X_bags[inst_index,:] for inst_index in range(X_bags.shape[0])]
+				#import pdb;pdb.set_trace()
+				predictions_accum = self._predict(iter = iter)
+
+				return np.array(predictions_accum)
+			else:
+			
+				#X_instances = np.vstack(X_bags)
+				predictions_accum = self._predict(iter = iter)
+				if getInstPrediction:  #return the instance level predictions for the input bags
+					return np.array(predictions_accum)
+				else:
+					predictions_bag = get_bag_label(predictions_accum, self.X_bags_test)
+					return predictions_bag
 		else:
+			raise Exception('As the first time to call predict(), please specify the test dataset')
 
-			X_instances = np.vstack(X_bags)
-			predictions_accum = self._predict(X_instances, iter)
-			predictions_bag = get_bag_label(predictions_accum, X_bags)
-			return predictions_bag
+
+
 
 def get_bag_label(instance_predictions, bags):
 	num_bag = len(bags)
