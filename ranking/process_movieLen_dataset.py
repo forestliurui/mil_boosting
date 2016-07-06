4bccb7eda82c35354c2c5fb8ca1c8801cc9f416a
Rui Liu <rui@chronos.eecs.cwru.edu>
2016-07-05 21:18:32 -0400

    add two more files ranking/process_movieLen_dataset is used to generate partitioned movieLen dataset from raw dataset rboost is robust boost

---
+++ ranking/process_movieLen_dataset.py
@@ -0,0 +1,206 @@
+"""
+This script is used to process the raw data of movieLen and partition it into appropriate training and test sets
+
+has passed tests, should be correct
+"""
+
+import unittest
+import csv
+import dill
+
+def process(filename):
+	#filename = "movieLen.csv"
+	
+
+	user_map, movie_map = getMap(filename)
+	
+	num_user = len(user_map)
+	num_movie = len(movie_map)
+
+	num_folds = 5
+
+	for index_user in range(num_user): #take turn to choose user for critical pairs
+		print "user index: ", index_user
+		if index_user >50: #try not to get too many subdatasets
+			break
+		X, y = getXy(user_map, movie_map, index_user)
+
+		X_train, y_train, X_test, y_test = getPartition(X, y, num_folds)
+
+		p_train= []
+		p_test = []
+		for temp in y_train:
+			p_train.append( getCriticalPair(temp) )
+		for temp in y_test:
+			p_test.append( getCriticalPair(temp) )
+	
+		movieLen = movieLenData(X_train, p_train, X_test, p_test)
+		
+		#import pdb;pdb.set_trace()
+		dill.dump(movieLen, open('ranking/movieLen/movieLen_user'+str(index_user)+'.pkl', 'wb'))
+
+class movieLenData(object):
+	def __init__(self, X_train, p_train, X_test, p_test):
+		self.X_train = X_train
+		self.p_train = p_train
+		
+		self.X_test = X_test
+		self.p_test = p_test
+
+
+
+def getCriticalPair(y):
+	"""
+	y is a dictionary like {movie_index1: 2, movie_index2: 1} for the index_user
+
+	return a list of tuples like [(movie_index1, movie_index2), (movie_index3, movie_index1) ]. For each tuple, the first item is assumed to be ranked higher than the second according to y
+	"""
+	
+	movieList = y.keys()
+	result = []
+	for i in movieList:
+		for j in movieList:
+			if y[i] > y[j]:
+				result.append((i, j))
+	return result
+
+
+def getPartition(X, y, num_folds):
+	"""
+	X as a dictionary like {movie_index1: list, movie_index2: list}, the list contains the rating of a specific movie from all other users except for the one in y. -1 is used to indicate missing value
+	y as a dictionary like {movie_index1: 2, movie_index2: 1} for the index_user
+	
+	num_folds is the num of folds to be partitioned into
+
+	return X_train, y_train, X_test, y_test
+	
+	X_train is a list of dic. The dic is like X. 
+	y_train is a list of dic. The dic is like y
+	...
+	Each fold corresponds to one item in lists
+	"""
+
+	num_movie = len(X)
+
+	X_train = []
+	y_train = []
+	X_test = []
+	y_test = []
+
+	for fold in range(num_folds):
+		ub = (fold+1)*(num_movie/num_folds) 
+		lb = fold*(num_movie/num_folds)
+
+		#import pdb;pdb.set_trace()
+		X_train.append({ key: list(X[key]) for key in X.keys() if key not  in range(lb, ub) })
+		X_test.append({ key: list(X[key]) for key in X.keys() if key  in range(lb, ub) })
+		y_train.append( { key: y[key] for key in y.keys() if key not in range(lb, ub) } )
+		y_test.append( { key: y[key] for key in y.keys() if key  in range(lb, ub) } )
+	return X_train, y_train, X_test, y_test
+
+def getMap(filename):
+	"""
+	file_name is a string respresenting a .csv file, each role of which is like user_id, movie_id, rating
+
+	return user_map and movie_map
+
+	user_map is dictionary to store the movie rating for each user, like {user1:{movie1: 2, movie2:3, movie3: 1}, user2: {movie1: 1, movie2:2, movie3: 1}  }
+	movie_map is dictionary to store the movie rating for each movie, like {movie1:{user1: 2, user2:3, user3: 1}, movie2: {user1: 1, user2:2, user3: 1}  }
+	"""
+
+	user_map = {}
+	movie_map = {}
+
+	with open(filename, "rb") as f:
+		reader = csv.reader(f, delimiter = ",")
+		for row in reader:
+			if row[0] not in user_map:
+				user_map[row[0]] = {}
+			user_map[row[0]][row[1]] = int(row[2])
+
+			if row[1] not in movie_map:
+				movie_map[row[1]] = {}
+			movie_map[row[1]][row[0]] = int(row[2])
+	return user_map, movie_map
+
+def getXy(user_map, movie_map, index_user):
+	"""
+	user_map is dictionary to store the movie rating for each user, like {user1:{movie1: 2, movie2:3, movie3: 1}, user2: {movie1: 1, movie2:2, movie3: 1}  }
+	movie_map is dictionary to store the movie rating for each movie, like {movie1:{user1: 2, user2:3, user3: 1}, movie2: {user1: 1, user2:2, user3: 1}  }
+
+	index_user is the index of user in user_map.keys() which will be used to construct critical pairs
+
+	return X as a dictionary like {movie_index1: list, movie_index2: list} , the list contains the rating of a specific movie from all other users except for the one in y. -1 is used to indicate missing value
+	      y as dictionary like {movie_index1: 2, movie_index2: 1} for the index_user
+
+	Processing is applied to make sure there is no missing value in y
+	"""	
+	X = {}
+	y = {}
+	id_user = user_map.keys()[index_user]
+
+	index_m_actual = -1
+	for index_m in range(len(movie_map)):
+		id_m = movie_map.keys()[index_m]
+
+		if id_user in movie_map[id_m]: #there is a rating for id_user/index_user for movie id_m
+			index_m_actual += 1
+			X[index_m_actual] = []
+		
+			for index_u in range(len(user_map)):
+				id_u = user_map.keys()[index_u]
+				if index_u!=index_user:
+					if id_u in movie_map[id_m]:
+						X[index_m_actual].append(  movie_map[id_m][id_u] )
+					else:
+						X[index_m_actual].append(-1)
+				else:
+					y[index_m_actual] = movie_map[id_m][id_u]
+			
+	return X, y
+
+
+
+class TestProcess(unittest.TestCase):
+	def DA_test_getMap(self):
+		file_name = "ranking/movieLen/test_process_movieLen.csv"
+		user_map, movie_map = getMap(file_name)		
+		print "user map"
+		print user_map
+
+		print "movie map"
+		print movie_map
+		
+	def DA_test_getXy(self):
+		file_name = "ranking/movieLen/test_process_movieLen.csv"
+		user_map, movie_map = getMap(file_name)	
+		print user_map.keys()[1]
+		X, y = getXy(user_map, movie_map, 1)
+		print X
+		print y
+
+	def DA_test_getPartition(self):
+		file_name = "ranking/movieLen/test_process_movieLen.csv"
+		user_map, movie_map = getMap(file_name)	
+		print user_map.keys()[1]
+		X, y = getXy(user_map, movie_map, 1)
+
+		X_train, y_train, X_test, y_test = getPartition(X, y, 2)
+		import pdb;pdb.set_trace()
+
+	def test_getCriticalPair(self):
+		file_name = "ranking/movieLen/test_process_movieLen.csv"
+		user_map, movie_map = getMap(file_name)	
+		print user_map.keys()[1]
+		X, y = getXy(user_map, movie_map, 1)
+
+		X_train, y_train, X_test, y_test = getPartition(X, y, 2)
+
+		p = getCriticalPair(y_train[0])
+		import pdb;pdb.set_trace()
+
+if __name__ == "__main__":
+	#unittest.main()
+	process("ranking/movieLen/movieLen.csv")
+
+
---
+++ src/rBoost_nondistributed.py
@@ -0,0 +1,327 @@
+"""
+This is the implementation of rBoost from 
+
+Bootkrajang, Jakramate, and Ata Kaban. "Boosting in the presence of label noise." Uncertainty in Artificial Intelligence. 2013.
+"""
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from scipy.optimize import minimize
+from numpy import sinh, exp
+import unittest
+
+WEAK_CLASSIFIERS = {
+	'dtree_stump': DecisionTreeClassifier,
+}
+
+class RBoost(object):
+	def __init__(self, **parameters):
+
+		self.max_iter_boosting = parameters.pop("max_iter_boosting", 10)
+		self.weak_classifier_name = parameters.pop('weak_classifier', 'dtree_stump') 
+		if self.weak_classifier_name == 'dtree_stump':
+			parameters['max_depth'] = 1
+		parameters.pop('normalization', 0)
+
+		self.parameters = parameters
+
+		self.X_bags_test = None
+		self.X_bags = None
+		self.y_labels = None
+		self.X_instances = None
+
+		self.instance_labels_generated_from_bag_labels = None
+		self.weak_classifiers = []
+		self.alphas = []
+		self.predictions_list_train = []
+
+	def fit(self, X_bags, y_labels):
+		'''
+		X_bags is either (1) a list of arrays, each bag is an array in the list -- multiple instance learning  or (2) a single array -- supervised learning
+		The row of array corresponds to instances in the bag, column corresponds to feature
+		y_labels is the list which contains the labels of bags. Here, binary labels are assumed, i.e. +1/-1
+		'''
+
+		if type(y_labels)!=list:
+			y_labels=y_labels.tolist()
+		
+		if type( y_labels[0] )==bool or 0 in y_labels:  #convert the boolean labels into +1/-1 labels
+			y_labels = map(lambda x:2*x-1, y_labels )
+		
+		max_iter_boosting=self.max_iter_boosting
+
+		if type(X_bags) == list:  # treat it as the SIL in MIL setting
+			num_bags=len(X_bags)
+				
+			num_instance_each_bag=[x.shape[0] for x in X_bags]
+		
+			instances=np.vstack((X_bags))
+			
+
+			instance_labels_generated_from_bag_labels=[y_labels[bag_index]*np.ones((1, num_instance_each_bag[bag_index]))[0] for bag_index in range(num_bags)  ]
+			instance_labels_generated_from_bag_labels=np.hstack((instance_labels_generated_from_bag_labels))
+
+			self.X_bags = X_bags
+			self.y_labels = y_labels 
+			self.X_instances = instances	
+			self.instance_labels_generated_from_bag_labels = instance_labels_generated_from_bag_labels		
+		else:
+			instances = X_bags
+			instance_labels_generated_from_bag_labels = y_labels
+
+			self.instance_labels_generated_from_bag_labels = instance_labels_generated_from_bag_labels
+
+			self.X_instances = instances
+
+
+		instance_labels_generated_from_bag_labels = np.array( instance_labels_generated_from_bag_labels )
+		num_instances = instances.shape[0]
+		num_instances_positive = np.sum(instance_labels_generated_from_bag_labels == 1)	
+		num_instances_negative = np.sum(instance_labels_generated_from_bag_labels != 1)		
+
+		# gamma[(j,k)] = p(\tilde{y} = k | y = j) i.e. the probablity of flipping true label y = j into observed label \tilde{y} = k
+		#initialization of gamma_jk
+		self.gamma = {}
+
+		self.gamma[(0,0)] = [0.7]
+		self.gamma[(0,1)] = [0.3]
+		
+		self.gamma[(1,0)] = [0] #true positive instance will always remain as true observed instance
+		self.gamma[(1,1)] = [1]
+		
+		self.w = {}
+		self.P  ={}
+		self.g = {}
+		for index_inst in range(num_instances):
+			self.w[index_inst] = {}
+			for i in [0,1]:
+				for j in [0,1]:
+					self.w[index_inst][(i,j)] = self.gamma[(i,j)][-1]
+
+		for index_Boosting in range(self.max_iter_boosting):
+			weights_instance = self.getInstWeight(instance_labels_generated_from_bag_labels, self.w)
+
+			#import pdb;pdb.set_trace()
+			instance_classifier=WEAK_CLASSIFIERS[self.weak_classifier_name](**self.parameters)
+			instance_classifier.fit(instances, instance_labels_generated_from_bag_labels, np.array(weights_instance) )
+			
+			self.predictions_list_train.append( instance_classifier.predict(instances) )
+			
+			weighted_error = np.average( instance_classifier.predict(instances) !=  self.predictions_list_train[-1], weights = weights_instance)
+			res = minimize(self.subproblem_to_update_alpha(instance_labels_generated_from_bag_labels, self.w,  weighted_error ), 0)	
+			alpha  = res['x'][0]
+			self.alphas.append(alpha)
+			self.weak_classifiers.append(instance_classifier)
+
+			#update self.w
+			for index_inst in range(num_instances):
+
+				self.w[index_inst][(0,0)] = self.gamma[(0,0)][-1]*exp(-self.predict(instances[index_inst,:].reshape((1,-1))))[0]*exp(2*alpha*( self.predictions_list_train[-1][index_inst]!=1 ))
+				self.w[index_inst][(0,1)] = self.gamma[(0,1)][-1]*exp(self.predict(instances[index_inst,:].reshape((1,-1))))[0]*exp(2*alpha*( self.predictions_list_train[-1][index_inst]==1 ))
+				self.w[index_inst][(1,0)] = self.gamma[(1,0)][-1]*exp(-self.predict(instances[index_inst,:].reshape((1,-1))))[0]*exp(2*alpha*( self.predictions_list_train[-1][index_inst]!=1 ))
+				self.w[index_inst][(1,1)] = self.gamma[(1,1)][-1]*exp(self.predict(instances[index_inst,:].reshape((1,-1))))[0]*exp(2*alpha*( self.predictions_list_train[-1][index_inst]==1 ))
+				#normalization on self.w
+				sum_w0 = sum([   self.w[index_inst][(0,0)], self.w[index_inst][(0,1)] ])
+				sum_w1 = sum([  self.w[index_inst][(1,0)], self.w[index_inst][(1,1)]  ])
+
+				self.w[index_inst][(0,0)] =  self.w[index_inst][(0,0)]/sum_w0
+				self.w[index_inst][(0,1)] =  self.w[index_inst][(0,1)]/sum_w0
+				
+				self.w[index_inst][(1,0)] =  self.w[index_inst][(1,0)]/sum_w1
+				self.w[index_inst][(1,1)] =  self.w[index_inst][(1,1)]/sum_w1
+
+			#update P Note: there are several ways to compute P. For simplicity, I use logistic calibratioin here
+			for index_inst in range(num_instances):
+				self.P[index_inst] = float(1)/(  1+exp(-self.predict(instances[index_inst,:].reshape((1,-1))))[0]  )
+			self.g[(1, 1)] = self.gamma[(1,1)][-1]*sum([  (instance_labels_generated_from_bag_labels[index_inst]==1)*self.P[index_inst]/(self.gamma[(1,1)][-1]*self.P[index_inst]+self.gamma[(0,1)][-1]*(1-self.P[index_inst]) )   for index_inst in range(num_instances)  ])
+			self.g[(1, 0)] = self.gamma[(1,0)][-1]*sum([  (instance_labels_generated_from_bag_labels[index_inst]!=1)*self.P[index_inst]/(self.gamma[(1,0)][-1]*self.P[index_inst]+self.gamma[(0,0)][-1]*(1-self.P[index_inst]) )   for index_inst in range(num_instances)  ])
+			self.g[(0, 1)] = self.gamma[(0,1)][-1]*sum([  (instance_labels_generated_from_bag_labels[index_inst]==1)*(1-self.P[index_inst])/(self.gamma[(1,1)][-1]*self.P[index_inst]+self.gamma[(0,1)][-1]*(1-self.P[index_inst]) )   for index_inst in range(num_instances)  ])
+			self.g[(0, 0)] = self.gamma[(0,0)][-1]*sum([  (instance_labels_generated_from_bag_labels[index_inst]!=1)*(1-self.P[index_inst])/(self.gamma[(1,0)][-1]*self.P[index_inst]+self.gamma[(0,0)][-1]*(1-self.P[index_inst]) )   for index_inst in range(num_instances)  ])
+
+			#update gamma
+			self.gamma[(0,0)].append( self.g[(0,0)]/(self.g[(0,0)]+self.g[(0,1)]) )
+			self.gamma[(0,1)].append( self.g[(0,1)]/(self.g[(0,0)]+self.g[(0,1)]) )		
+			self.gamma[(1,0)].append( self.g[(1,0)]/(self.g[(1,0)]+self.g[(1,1)]) )
+			self.gamma[(1,1)].append( self.g[(1,1)]/(self.g[(1,0)]+self.g[(1,1)]) )
+
+					
+	def predict_train(self, iter = None, getInstPrediction = False):
+
+
+		self.c = self.alphas
+		threshold = 0.5
+		if iter == None or iter > len(self.c):
+			iter = len(self.c)
+
+		predictions_accum = np.matrix(self.c[0:iter])*np.matrix( np.vstack((self.predictions_list_train[0:iter])) )/np.sum(self.c[0:iter])
+		results = np.array(predictions_accum)[0] - threshold
+
+		if getInstPrediction:
+			return results
+		else: #get bag level predictions
+			if self.X_bags is None:
+				raise Exception("Can't get bag level prediction for training data due to the lack of training bag data")
+			else:
+				predictions_bag = get_bag_label(results, self.X_bags)
+				return predictions_bag
+
+	def _predict(self, X = None, iter = None):
+		"""
+		X is assumed to be two dimensional array, each row corresponding to an instance
+		"""
+		
+		self.c = self.alphas
+		threshold = 0.5
+
+		if iter == None or iter > len(self.c):
+			iter = len(self.c)
+		if X is not None:	
+
+			predictions_list = [( instance_classifier.predict(X).reshape((1, -1))>0 )+ 0 for instance_classifier in self.weak_classifiers ]
+			self.predictions_list_test = predictions_list
+			#import pdb;pdb.set_trace()
+			predictions_accum = np.matrix(self.c[0:iter])*np.matrix( np.vstack((predictions_list[0:iter])) )/np.sum(self.c[0:iter])
+
+			#import pdb;pdb.set_trace()
+			return np.array(predictions_accum)[0] - threshold   #we need to deduct a threshold because (instance_classifier.predict > 0) + 0 is either 0 or 1
+		else:
+			predictions_accum = np.matrix(self.c[0:iter])*np.matrix( np.vstack((self.predictions_list_test[0:iter])) )/np.sum(self.c[0:iter])
+
+			#import pdb;pdb.set_trace()
+			return np.array(predictions_accum)[0] - threshold   #we need to deduct a threshold because (instance_classifier.predict > 0) + 0 is either 0 or 1
+
+	def predict(self, X_bags = None, iter = None, getInstPrediction = False):
+		#X_bags is a list of arrays, each bag is an array in the list
+		#The row of array corresponds to instances in the bag, column corresponds to feature
+
+		#predictions_bag is the returned array of predictions which are real values 
+		
+		self.c = self.alphas
+		if iter == None or iter > len(self.c):
+			iter = len(self.c)
+	
+		#print "self.c: ",
+		#print len(self.c)
+		if X_bags is not None:
+			self.X_bags_test = X_bags
+
+			if type(X_bags) != list:  # treat it as normal supervised learning setting
+				#X_bags = [X_bags[inst_index,:] for inst_index in range(X_bags.shape[0])]
+				return self._predict(X = X_bags, iter = iter)
+				
+			else:
+
+				X_instances = np.vstack(X_bags)
+				predictions_accum = self._predict(X = X_instances, iter =  iter)
+				if getInstPrediction:  #return the instance level predictions for the input bags
+					return predictions_accum
+				else:
+					predictions_bag = get_bag_label(predictions_accum, X_bags)
+					return predictions_bag
+
+		elif X_bags is None and self.X_bags_test is not None:
+			if type(self.X_bags_test) != list:  # treat it as normal supervised learning setting
+				#X_bags = [X_bags[inst_index,:] for inst_index in range(X_bags.shape[0])]
+				#import pdb;pdb.set_trace()
+				predictions_accum = self._predict(iter = iter)
+
+				return np.array(predictions_accum)
+			else:
+			
+				#X_instances = np.vstack(X_bags)
+				predictions_accum = self._predict(iter = iter)
+				if getInstPrediction:  #return the instance level predictions for the input bags
+					return np.array(predictions_accum)
+				else:
+					predictions_bag = get_bag_label(predictions_accum, self.X_bags_test)
+					return predictions_bag
+		else:
+			raise Exception('As the first time to call predict(), please specify the test dataset')
+
+
+	def subproblem_to_update_alpha(self, labels, w, weighted_error):
+		"""
+		labels is one-D array of labels
+		w is a dictionary containing w[(0,0)], w[(0,1)], w[(1,0)], w[(1,1)]
+
+		weighted_error is a real number, which indicates the traning error of current weak classifier weighted by weights_instance
+		"""
+
+		term1 = 0
+		term2 = 0
+		for i in range(labels.shape[0]):
+			if labels[i] == 1:
+				term1 += w[i][(0,0)]
+				term2 += w[i][(0,1)]
+			else:
+				term1 += w[i][(1,1)]
+				term2 += w[i][(1,0)]
+
+
+		return lambda x: 2*sinh(x)*weighted_error + exp(-x)*term1+exp(x)*term2
+		
+
+
+	def getInstWeight(self, labels, w):
+		"""
+		labels is one-D array of labels. Here, binary labels are assumed, i.e. +1/-1
+		w is dictionary, which maps keys to real values. Key is taken value from [(0,0), (0,1), (1, 0), (1,1)]
+
+
+		return the instance-level weights as a list
+		"""
+
+		result = []
+		
+		for i in range(labels.shape[0]):
+			
+			if labels[i] == 1:
+				result.append(w[i][(0,0)] - w[i][(0, 1)])
+			else:
+				result.append(w[i][(1,1)] - w[i][(1, 0)])
+		return result
+
+def get_bag_label(instance_predictions, bags):
+	num_bag = len(bags)
+	p_index= 0
+	bag_predictions = []
+	for bag_index in range(num_bag):
+		n_index =p_index+ bags[bag_index].shape[0]
+		
+		bag_predictions.append( np.max(instance_predictions[p_index: n_index]) )
+		p_index = n_index
+	return np.array(bag_predictions)
+
+class TestRBoostFitMethod(unittest.TestCase):
+	def no_test_fit1(self):
+		X_bags = [np.array([[-1, 0]]), np.array([[1,0]]), np.array([[0, 1]]), np.array([[0, -1]])]
+		y_labels = [-1, -1, 1, 1]
+		param = {"max_iter_boosting": 50}
+		rbooster = RBoost(**param)
+
+		rbooster.fit(X_bags, y_labels)
+		print rbooster.predict(X_bags)
+		#import pdb;pdb.set_trace()
+	def test_fit1(self):
+		X_bags = [np.array([[-1, 1],[1, 1]]), np.array([[-2,1],[-2, -1]]), np.array([[2, 1], [2, -1]]), np.array([[-1, -1], [1, -1]])]
+		y_labels = [-1, 1, 1, 1]
+		param = {"max_iter_boosting": 100}
+		rbooster = RBoost(**param)
+
+		rbooster.fit(X_bags, y_labels)
+		print rbooster.predict(X_bags)
+		
+
+		X_bags_test= [np.array([[0, 1]]), np.array([[0, 2]]), np.array([[0, -1]]), np.array([[0, -2]]), np.array([[2, 0]]), np.array([[3, 0]]), np.array([[-2, 0]]), np.array([[-3, 0]])]
+		print rbooster.predict(X_bags_test)
+		import pdb;pdb.set_trace()
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+
+
+
+
+
