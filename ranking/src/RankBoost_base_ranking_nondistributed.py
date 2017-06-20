fix more bugs on computation of E1 and E2. Could pass the test on one single fold 0-2 MovieLen dataset, using the theoretical fact that (1) E1 is always equal when using exponential and vanilla scaling function for any algorithm (2) E1 is equal when using exponential, vanilla scaling function and Z for vanilla rankboost (3) E is equal when using modified scaling function and modified Z for rankboost+ by Harold

ranking/src/RankBoost_base_ranking_nondistributed.py
@@ -53,6 +53,9 @@ class RankBoost_base_ranking(object):
 		self.epsilon_pair_fast["negative"] = []
 		self.epsilon_pair_fast["zero"] = []
 
+                self.E1_bound = E1()
+                self.E2_bound = E2()
+
 	def fit(self, X, y):
 		'''
 		X is a hashtable with key being instance ID, value being the one-dimensional array containing its features
@@ -214,7 +217,7 @@ class RankBoost_base_ranking(object):
 		bound = np.exp( -2*( np.sum( ((epsilon_pos - epsilon_neg)/2 )**2 ) )  )
 		return bound
 
-        def getE_vanilla(self, iter = None):
+        def getE_vanilla_exp(self, iter = None):
                 """
                 return the E for vanilla rankboost, which is defined as E = (1/m)*( \sum_{i=1}^{m} exp(-y_i (g(x'_i) - g(x_i))) ),
                    where m is the number of examples, and g(x) is the predicted value for the example x, and y_i is the label for i-th pair
@@ -228,6 +231,49 @@ class RankBoost_base_ranking(object):
                 
                 return np.mean(res)
 
+        def getE_vanilla(self, iter = None):
+                """
+                return E, based on the formula, (1/m)*\sum_{i=1}^m \prod_{s=1}^t scale_s(i)
+                where scale_s(i) is defined as:
+                   (1) for crankboost:
+                       scale_s(i) = e**(-alpha_s) if pair i is correctly ranked
+                                  = e**(alpha_s)  if pair i is reverse ranked
+                                  = cosh(alpha_s) if pair i is tied
+                   (2) for rankboost vanilla:
+                       scale_s(i) = e**(-alpha_s) if pair i is correctly ranked
+                                  = e**(alpha_s)  if pair i is reverse ranked
+                                  = 1             if pair i is tied
+                Note: iter starts from 1
+                """
+
+                self.E1_bound.setAll(rankers = self.weak_classifiers, alphas = self.alphas, epsilons = self.epsilon)
+
+                if iter is None or iter > len(self.alphas):
+                      iter = len(self.alphas)
+                res_pair = {}
+
+                for iteration in range(iter):
+                     predictions = self.predict_train_weak_ranker(iteration)
+                     for pair in self.y_train:
+                         if pair not in res_pair:
+                             res_pair[pair] = 1
+                         ordering = self.comparePairPredictions( predictions[pair[0]], predictions[pair[1]] )
+                         res_pair[pair] *= self.E1_bound.getScale(iteration, ordering = ordering)
+                return np.mean(res_pair.values())
+
+        def getE_Z_vanilla(self, iter = None):
+                """
+                return E, which is the upper bound for 0-1 loss, based on the assumption that E( \sum_{s=1}^{t} alpha_s*h_s ) = \prod_{s=1}^{t} Z_s
+                Note that for vanilla rankboost, E is actually exponential function. However, E might take another form for other variants of rankboost algorithms.
+                Note that iter here means the number of boosting rounds/iterations that we checked.
+                """
+                self.E1_bound.setAll(rankers = self.weak_classifiers, alphas = self.alphas, epsilons = self.epsilon)
+                if iter == None or iter > len(self.alphas):
+                     iter = len(self.alphas)
+
+                Z = self.E1_bound.getZ()
+                E = reduce(lambda x, y: x*y, Z[:iter])
+                return E
 
 	def getE_Z(self, iter = None):
                 """
@@ -235,12 +281,14 @@ class RankBoost_base_ranking(object):
                 Note that for vanilla rankboost, E is actually exponential function. However, E might take another form for other variants of rankboost algorithms.
                 Note that iter here means the number of boosting rounds/iterations that we checked.
                 """
-
+                self.E2_bound.setAll(rankers = self.weak_classifiers, alphas = self.alphas, epsilons = self.epsilon)
                 if iter == None or iter > len(self.alphas):
                      iter = len(self.alphas)
 
-                E = reduce(lambda x, y: x*y, self.Z[:iter])
+                Z = self.E2_bound.getZ()
+                E = reduce(lambda x, y: x*y, Z[:iter])
                 return E
+
         def getE(self, iter = None):
                 """
                 return E, based on the formula, (1/m)*\sum_{i=1}^m \prod_{s=1}^t scale_s(i)
@@ -255,6 +303,7 @@ class RankBoost_base_ranking(object):
                                   = 1             if pair i is tied
                 Note: iter starts from 1
                 """
+                self.E2_bound.setAll(rankers = self.weak_classifiers, alphas = self.alphas, epsilons = self.epsilon)
                 if iter is None or iter > len(self.alphas):
                       iter = len(self.alphas)
                 res_pair = {}
@@ -265,7 +314,7 @@ class RankBoost_base_ranking(object):
                          if pair not in res_pair:
                              res_pair[pair] = 1
                          ordering = self.comparePairPredictions( predictions[pair[0]], predictions[pair[1]] )
-                         res_pair[pair] *= self.scale(iteration, ordering = ordering)
+                         res_pair[pair] *= self.E2_bound.getScale(iteration, ordering = ordering)
                 return np.mean(res_pair.values())
         
         def comparePairPredictions(self, prediction1, prediction2  ):
@@ -335,8 +384,8 @@ class RankBoost_base_ranking(object):
 			iter = len(self.c)
 		results = {}
 		for inst_ID in self.X_train.keys():
-			results[inst_ID] = np.average( [self.predictions_list_train[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
-
+			#results[inst_ID] = np.average( [self.predictions_list_train[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
+                        results[inst_ID] = np.average( [self.predictions_list_train[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*sum(self.c[0:iter])
 		return results
 
 	def predict(self, X = None, iter = None):
@@ -355,14 +404,119 @@ class RankBoost_base_ranking(object):
 			self.predictions_list_test = predictions_list
 			results = {}
 			for inst_ID in X.keys():
-				results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
-			return results
+				#results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
+			        results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*sum(self.c[0:iter])
+                        return results
 		else:
 			results = {}
 			for inst_ID in self.X_test.keys():
-				results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
-			return results
+				#results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*np.sign(sum(self.c[0:iter]))
+			        results[inst_ID] = np.average( [self.predictions_list_test[index][inst_ID] for index in range(iter) ] , weights = self.c[0:iter]   )*sum(self.c[0:iter])
+                        return results
+class E_base(object):
+     def __init__(self):
+         self.rankers = []
+         self.epsilons = []
+         self.alphas = []
+         self.Z = []
+     def setAll(self, rankers = None, epsilons = None, alphas = None):
+         if rankers is not None:
+            self.rankers = rankers
+         if epsilons is not None:
+            self.epsilons = epsilons
+         if alphas is not None:
+            self.alphas = alphas        
 
+     def setRankers(self, rankers):
+         self.rankers = rankers
+
+     def setEpsilons(self, epsilons):
+         #epsilons is a list of dict, each element is a dict, with keys being 'positive', 'negative' and 'zero'
+         self.epsilons = epsilons
+     def setAlphas(self, alphas):
+         self.alphas = alphas
+
+     def getZ(self):
+          self.update()
+          #E = reduce(lambda x, y: x*y, self.Z[:iter])
+          return self.Z
+     def update(self):
+         raise NotImplementedError("member function update is NOT implemented in the base class")
+
+     def getScale(self, iteration, ordering = 0):
+         raise NotImplementedError("member function getScale is NOT implemented in the base class")
+
+class E1(E_base):
+     def __init__(self):
+         super(E1, self).__init__()
+     def update(self):
+
+         if len(self.rankers) != len(self.alphas):
+                raise ValueError("the lengths of rankers, epsilons and alphas are NOT equal!")
+
+         start_index = len(self.Z)
+         end_index = len(self.alphas)
+
+         for index in range(start_index, end_index):
+             cur_alpha = self.alphas[index]
+             z = self.epsilons["positive"][index]*np.exp(-cur_alpha) + self.epsilons["negative"][index]*np.exp(cur_alpha) + self.epsilons["zero"][index]
+             self.Z.append(z)
+
+     def getScale(self, iteration, ordering = 0):
+            """
+            iteration starts from 0
+            """
+            self.update()
+            if ordering == 1:
+               return np.exp(-self.alphas[iteration])
+            elif ordering == -1:
+               return np.exp(self.alphas[iteration])
+            else:
+               return 1.0
+
+class E2(E_base):
+     def __init__(self):
+         super(E2, self).__init__()
+         
+         self.pre_alphas = [] #total alphas for previous identical rankers
+         self.alpha_dict = {}
+
+     def update(self):     
+         #import pdb;pdb.set_trace() 
+         if len(self.rankers) != len(self.alphas):
+                raise ValueError("the lengths of rankers, epsilons and alphas are NOT equal!")  
+         if len(self.Z) != len(self.pre_alphas):
+                raise ValueError("the lengths of Z and pre_alphas are NOT equal!")         
+
+         start_index = len(self.Z)
+         end_index = len(self.alphas)
+
+         for index in range(start_index, end_index):
+             ranker = self.rankers[index]
+             if ranker not in self.alpha_dict:
+                pre_alpha = 0
+             else:
+                pre_alpha = self.alpha_dict[ranker]
+             cur_alpha = self.alphas[index]
+             new_pre_alpha = pre_alpha + cur_alpha
+             self.alpha_dict[ranker] = new_pre_alpha
+             self.pre_alphas.append(pre_alpha)
+             z = self.epsilons["positive"][index]*np.exp(-cur_alpha) + self.epsilons["negative"][index]*np.exp(cur_alpha) + self.epsilons["zero"][index]*np.cosh(new_pre_alpha)/np.cosh(pre_alpha)
+             self.Z.append(z)
+
+     def getScale(self, iteration, ordering = 0):
+            """
+            iteration starts from 0
+            """
+            self.update()
+            if ordering == 1:
+               return np.exp(-self.alphas[iteration])
+            elif ordering == -1:
+               return np.exp(self.alphas[iteration])
+            else:       
+               return np.cosh(self.alphas[iteration]+self.pre_alphas[iteration])/np.cosh( self.pre_alphas[iteration] )
+        
+    
 
 def get_bag_label(instance_predictions, bags):
 	num_bag = len(bags)

ranking/src/RankBoost_modiVI_ranking_nondistributed.py
@@ -28,8 +28,11 @@ class RankBoost_modiVI_ranking(RankBoost_base_ranking):
       def __init__(self, **parameters):
 
             self.Z = []
+            self.Z_vanilla = []
             super(RankBoost_modiVI_ranking, self).__init__(**parameters)
-            self.new_alphas = []
+            self.pre_alphas = []           
+
+ 
       def fit(self, X, y):
             '''
 	    X is a hashtable with key being instance ID, value being the one-dimensional array containing its features
@@ -85,18 +88,20 @@ class RankBoost_modiVI_ranking(RankBoost_base_ranking):
                       new_alpha = sys.maxint
                 else:
                       new_alpha = 0.5*np.log( float(numerator)/denominator )
-                self.alphas_dict[instance_classifier] = new_alpha #i.e. the total alpha for any instance so far            
+                self.alphas_dict[instance_classifier] = new_alpha + pre_alpha #i.e. the total alpha for any instance so far            
                 print("iter: %d, weak ranker: %d"%( index_Boosting, len(self.alphas_dict) ))   
-                ad_alpha = new_alpha - pre_alpha
-                self.alphas.append(ad_alpha)
-                self.new_alphas.append(new_alpha)
+                #ad_alpha = new_alpha - pre_alpha
+                self.alphas.append(new_alpha)
+                self.pre_alphas.append( pre_alpha )
+                #self.new_alphas.append(new_alpha)
 
                 if numerator == 0 or denominator == 0:
                      break
                 
                 cur_Z = epsilon_pos*np.exp(-new_alpha) + epsilon_neg*np.exp(new_alpha) + epsilon0*(np.cosh(new_alpha+pre_alpha))/np.cosh(pre_alpha)
-
+                cur_Z_vanilla = epsilon_pos*np.exp(-new_alpha) + epsilon_neg*np.exp(new_alpha) + epsilon0
                 self.Z.append(cur_Z)  
+                self.Z_vanilla.append(cur_Z_vanilla)
 
                 for pair in y:
                      r0 = 2*predictions[pair[0]] - 1
@@ -112,16 +117,19 @@ class RankBoost_modiVI_ranking(RankBoost_base_ranking):
       
             self.actual_rounds_of_boosting = len(self.alphas)
 
-      def scale(self, iteration, ordering = 0):
+      def scale(self, iteration, ordering = 0, isVanilla = False):
             """
             iteration starts from 0
             """
             if ordering == 1:
-               return np.exp(-self.new_alphas[iteration])
+               return np.exp(-self.alphas[iteration])
             elif ordering == -1:
-               return np.exp(self.new_alphas[iteration])
+               return np.exp(self.alphas[iteration])
             else:
-               return np.cosh(self.new_alphas[iteration]) 
+               if isVanilla is False:
+                  return np.cosh(self.alphas[iteration]+self.pre_alphas[iteration])/np.cosh( self.pre_alphas[iteration] ) 
+               else:
+                  return 1.0
 
 class TestRankBoost_ModiVI(unittest.TestCase):
     def no_test1(self):